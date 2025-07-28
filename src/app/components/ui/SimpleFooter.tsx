bb3da95ae505fae3f710134c7822bdb45a6ac193
joules19 <babafemiayodele@gmail.com>
2025-07-28 15:35:24 +0100

    removed address

--- src/app/components/ui/SimpleFooter.tsx
+++ src/app/components/ui/SimpleFooter.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { Facebook, Twitter, Instagram, Linkedin, Mail, Phone, MapPin } from "lucide-react";
+import { Facebook, Twitter, Instagram, Linkedin, Mail, Phone } from "lucide-react";
 import chowmateLogo from "../../assets/images/chowmate-light-mont.png";
 
 const socialLinks = [
